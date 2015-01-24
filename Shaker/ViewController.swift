f7d49150a7ef3e86d4a135b1943c3c1f0ac147c8
akkshay <akkshaykhoslaa@gmail.com>
2015-01-24 12:53:48 -0800

    Shaker
    A simple iOS app that plays a random sound every time the phone is
    shaken, made as part of an iOS course I took.

--- Shaker/ViewController.swift
+++ Shaker/ViewController.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var soundfiles = ["exhale","gun","halloween","helicopter","punch","railroad","roar"]
+    var player1:AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        
+        if event.subtype == UIEventSubtype.MotionShake {
+            
+            var randomNum:Int = Int(arc4random_uniform(UInt32(soundfiles.count)))
+            
+            var fileLocation = NSString(string:NSBundle.mainBundle().pathForResource(String(soundfiles[randomNum]), ofType: "mp3")!)
+            
+            var error:NSError? = nil
+            
+            player1 = AVAudioPlayer(contentsOfURL: NSURL(string: fileLocation), error: &error)
+            
+            player1.play()
+            
+        }
+    
+    
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
